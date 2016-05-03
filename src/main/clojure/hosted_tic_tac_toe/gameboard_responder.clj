d58d82135a65556076052753788940f5413fc946
Cat McLoughlin <cmcloughlin@8thlight.com>
2016-05-03 15:01:34 -0700

    PUT request with valid choice, board, and marker returns updated board in HTML

--- src/main/clojure/hosted_tic_tac_toe/gameboard_responder.clj
+++ src/main/clojure/hosted_tic_tac_toe/gameboard_responder.clj
@@ -2,29 +2,50 @@
   (:require [tictactoe.board :as board]))
 
 (import '(responders Responder)
-        '(http_messages HTTPStatus HTMLContent Response$ResponseBuilder Header$HeaderBuilder Header ResponseHeader))
+        '(http_messages HTTPStatus HTMLContent Request
+                        Response$ResponseBuilder Header$HeaderBuilder
+                        Header ResponseHeader))
 
 (def page-name "Game Board")
 
-(def supported-methods ["GET"])
+(def supported-methods ["GET" "PUT"])
 
 (defn- request-is-supported [request]
   (not (nil? (some #{(.getMethod request)} supported-methods))))
 
 (defn- get-status-line [request]
-  (do
-    (if (request-is-supported request)
-      (.getStatusLine HTTPStatus/OK)
-      (.getStatusLine HTTPStatus/METHOD_NOT_ALLOWED))))
+  (if (request-is-supported request)
+    (.getStatusLine HTTPStatus/OK)
+    (.getStatusLine HTTPStatus/METHOD_NOT_ALLOWED)))
 
 (defn- get-headers []
   (let [content-type (.build (Header$HeaderBuilder. (str (.getKeyword (ResponseHeader/CONTENT_TYPE)) (HTMLContent/contentType) ";")))]
     (into-array Header [content-type])))
 
+(defn- get-board-data [request]
+  (let [board (.getData request)]
+      board))
+
+(defn parse-board-data [board-string]
+  (let [symbolic-board (map read-string (clojure.string/split (clojure.string/replace board-string #"\[|\]" "") #" "))]
+    (vec (map #(if (not (number? %)) (name %) %) symbolic-board))))
+
+(defn- update-board [request-data]
+  (let [lines (clojure.string/split request-data #"\r\n")]
+    (let [board-string (first lines)]
+      (let [current-board (parse-board-data board-string)]
+        (let [choice (read-string (second lines))]
+          (board/fill-space choice (last lines) current-board))))))
+
 (defn- get-body [request]
   (if (request-is-supported request)
-    (str (HTMLContent/openHTMLAndBody page-name) (board/make-board) (HTMLContent/closeBodyAndHTML))
-    nil))
+    (cond
+      (= (.getMethod request) "GET")
+        (str (HTMLContent/openHTMLAndBody page-name) (board/make-board) (HTMLContent/closeBodyAndHTML))
+      (= (.getMethod request) "PUT")
+        (str (HTMLContent/openHTMLAndBody page-name) (update-board (.getData request)) (HTMLContent/closeBodyAndHTML))
+      :else "")
+    ""))
 
 (defn get-board-response [request]
   (let [status-line (get-status-line request)]
--- src/test/clojure/hosted_tic_tac_toe/gameboard_responder_test.clj
+++ src/test/clojure/hosted_tic_tac_toe/gameboard_responder_test.clj
@@ -3,37 +3,54 @@
             [hosted-tic-tac-toe.gameboard-responder :as responder]
             [tictactoe.board :as board]))
 
-(import '(http_messages Response Request$RequestBuilder HTTPStatus ResponseHeader HTMLContent))
+(import '(http_messages Response Request Request$RequestBuilder HTTPStatus ResponseHeader HTMLContent))
 
-(defn valid-gameboard-request []
-  (.build (Request$RequestBuilder. "GET /gameboard")))
+(def updated-board (board/fill-space 1 "X" (board/make-board)))
+
+(def get-request (.build (Request$RequestBuilder. (str "GET /gameboard"))))
+
+(def invalid-get-request (.build (Request$RequestBuilder. "POST /gameboard")))
+
+(def put-data (str "[0 1 2 3 4 5 6 7 8]" (Request/newLine) "1" (Request/newLine) "X"))
+
+(def valid-put-request
+  (.build (Request$RequestBuilder. (str "PUT /gameboard" (Request/newLine) (Request/newLine) put-data))))
 
 (defn response-body [request]
   (.getBody (responder/get-board-response request)))
 
-(defn invalid-gameboard-request []
-  (.build (Request$RequestBuilder. "POST /gameboard")))
-
 (deftest returns-response-object
-  (is (= Response (type (responder/get-board-response (valid-gameboard-request))))))
+  (is (= Response (type (responder/get-board-response get-request)))))
 
 (deftest valid-response-code-is-200
-  (is (= (.getStatusLine HTTPStatus/OK) (.getStatusLine (responder/get-board-response (valid-gameboard-request))))))
+  (is (= (.getStatusLine HTTPStatus/OK) (.getStatusLine (responder/get-board-response get-request)))))
 
 (deftest invalid-response-code-is-405
-  (is (= (.getStatusLine HTTPStatus/METHOD_NOT_ALLOWED) (.getStatusLine (responder/get-board-response (invalid-gameboard-request))))))
+  (is (= (.getStatusLine HTTPStatus/METHOD_NOT_ALLOWED) (.getStatusLine (responder/get-board-response invalid-get-request)))))
 
 (deftest response-has-html-header
-  (is (= (.getLine (get (.getHeaders (responder/get-board-response (valid-gameboard-request))) 0))
+  (is (= (.getLine (get (.getHeaders (responder/get-board-response get-request)) 0))
          (str (.getKeyword ResponseHeader/CONTENT_TYPE) (HTMLContent/contentType) ";"))))
 
-(deftest valid-request-returns-html-template
-    (is (true? (and (.contains (response-body (valid-gameboard-request)) (HTMLContent/openHTMLAndBody responder/page-name))
-                    (.contains (response-body (valid-gameboard-request)) (HTMLContent/closeBodyAndHTML))))))
+(deftest valid-get-request-returns-html-template
+  (is (true? (and (.contains (response-body get-request) (HTMLContent/openHTMLAndBody responder/page-name))
+                  (.contains (response-body get-request) (HTMLContent/closeBodyAndHTML))))))
 
-(deftest valid-request-html-response-has-empty-board
-  (is (true? (.contains (response-body (valid-gameboard-request)) (str (board/make-board))))))
+(deftest get-gameboard-returns-empty-board
+  (is (true? (.contains (response-body get-request) (str (board/make-board))))))
 
-(deftest invalid-request-does-not-get-response-body
-  (is (empty? (response-body (invalid-gameboard-request)))))
+(deftest invalid-get-request-gets-empty-response-body
+  (is (empty? (response-body invalid-get-request))))
 
+(deftest valid-put-response-code
+  (is (= (.getStatusLine HTTPStatus/OK) (.getStatusLine (responder/get-board-response valid-put-request)))))
+
+(deftest put-response-has-html-header
+  (is (= (.getLine (get (.getHeaders (responder/get-board-response valid-put-request)) 0))
+         (str (.getKeyword ResponseHeader/CONTENT_TYPE) (HTMLContent/contentType) ";"))))
+
+(deftest parses-and-formats-request-board-for-evalueation
+  (is (= (vec '("X" 1 2 3 4 5 6 7 8)) (responder/parse-board-data "[X 1 2 3 4 5 6 7 8]"))))
+
+(deftest valid-put-updates-board
+  (is (true? (.contains (response-body valid-put-request) (str updated-board)))))
