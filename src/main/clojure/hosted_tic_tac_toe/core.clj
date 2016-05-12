4c61e5ab9f814fe0013a13ed0add2dfd0fcd6288
Cat McLoughlin <cmcloughlin@8thlight.com>
2016-05-12 13:30:13 -0700

    Changes to comply with updated javaserver

--- src/main/clojure/hosted_tic_tac_toe/core.clj
+++ src/main/clojure/hosted_tic_tac_toe/core.clj
@@ -1,9 +1,10 @@
 (ns hosted-tic-tac-toe.core
   (:require [tictactoe.console :as console]
-            [hosted-tic-tac-toe.ttt-routes :as routes])
+            [hosted-tic-tac-toe.ttt-router :as router])
   (:gen-class))
 
-(import '(javaserver Router Server)
+(import '(routers Router)
+        '(javaserver Server)
         '(text_parsers ArgParser)
         '(factories ServerCreator))
 
@@ -12,11 +13,11 @@
 (defn- get-port-choice [args]
   (ArgParser/getPortChoice (into-array String args) default-port))
 
-(defn get-router [routes]
-  (Router. routes))
-
 (defn get-server [port router]
   (.createServer (ServerCreator.) port router))
+
+(defn get-ttt-router []
+  (router/new-ttt-router))
 
 (defn run-server [server]
   (.run server))
@@ -25,6 +26,6 @@
   [& args]
   (let [port-choice (get-port-choice args)]
     (println (str "Server running on port " port-choice))
-    (let [router (get-router (routes/get-routes))]
+    (let [router (get-ttt-router)]
       (let [server (get-server port-choice router)]
         (run-server server)))))
--- src/main/clojure/hosted_tic_tac_toe/ttt_router.clj
+++ src/main/clojure/hosted_tic_tac_toe/ttt_router.clj
@@ -1,13 +1,18 @@
-(ns hosted-tic-tac-toe.ttt-routes
+(ns hosted-tic-tac-toe.ttt-router
   (:require [tictactoe.board :as board]
             [hosted-tic-tac-toe.gameboard-responder :as gameboard-responder]
-            [hosted-tic-tac-toe.end-game-responder :as end-game-responder])
-  (:gen-class))
+            [hosted-tic-tac-toe.end-game-responder :as end-game-responder]))
 
 
-(import '(responders RedirectResponder))
+(import '(routers Router)
+        '(responders RedirectResponder))
 
-(defn get-routes []
+(defn get-routes-and-responders []
     (java.util.HashMap. {"/" (RedirectResponder. (into-array String ["GET"]) "/gameboard")
                          "/gameboard" (gameboard-responder/new-gameboard-responder)
                          "/game-over" (end-game-responder/new-end-game-responder)}))
+
+(defn new-ttt-router []
+  (reify
+    Router
+      (getRoutesAndResponders [this] (get-routes-and-responders))))
